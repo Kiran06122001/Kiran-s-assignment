9cb6c7935d1bba10849f4c5494d37e78abf7038a
Kiran06122001 <124714298+Kiran06122001@users.noreply.github.com>
2023-10-20 13:02:37 +0530

    Create B.java

---
+++ B.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+
+public class RomanToInteger {
+    public static void main(String[] args) {
+        String romanNumeral = "IX"; // Change this to the Roman numeral you want to convert
+        int result = romanToInt(romanNumeral);
+        System.out.println(result);
+    }
+
+    public static int romanToInt(String s) {
+        HashMap<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int currentValue = romanMap.get(s.charAt(i));
+            if (currentValue < prevValue) {
+                result -= currentValue;
+            } else {
+                result += currentValue;
+            }
+            prevValue = currentValue;
+        }
+
+        return result;
+    }
+}
